e2df64cc24bae82b6ae694a70677bb0358eff527
secretceremony <ansellmatita@gmail.com>
2025-06-14 17:19:26 +0700

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/invoicez/' : '/',
+ base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
